a9589b0d76401c7a17996c72ac1f1cd6b6c51726
German <gogapopp@gmail.com>
2024-04-18 01:47:12 +0300

    feat: server changes

--- cmd/main.go
+++ cmd/main.go
@@ -10,55 +10,39 @@ import (
 	"github.com/go-chi/httprate"
 	"github.com/gogapopp/Skoof/internal/config"
 	"github.com/gogapopp/Skoof/internal/handler"
-	"github.com/gogapopp/Skoof/internal/handler/middlewares"
 	"github.com/gogapopp/Skoof/internal/lib/logger"
 	"github.com/gogapopp/Skoof/internal/service"
 	"github.com/gogapopp/Skoof/internal/storage/postgres"
 )
 
 func main() {
-	logger, err := logger.New()
-	if err != nil {
-		logger.Fatal(err)
-	}
-	config, err := config.New(".env")
-	if err != nil {
-		logger.Fatal(err)
-	}
+	var (
+		logger = must(logger.New())
+		config = must(config.New(".env"))
+		db     = must(postgres.New(config.PGConfig.DSN))
 
-	db, err := postgres.New(config.PGConfig.DSN)
-	if err != nil {
-		log.Fatal(err)
-	}
+		authService = service.New(config.PASS_SECRET, config.JWT_SECRET, db)
+		r           = chi.NewRouter()
+	)
 	defer db.Close()
-
-	service := service.New(config.PASS_SECRET, config.JWT_SECRET, db)
-
-	r := chi.NewRouter()
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.Logger)
-	r.Use(httprate.Limit(3, time.Second))
-
-	r.Get("/", handler.HomePage(logger))
-
-	r.Get("/signin", handler.SignInPage(logger, service))
-	r.Post("/signin", handler.SignInPage(logger, service))
-
-	r.Get("/signup", handler.SignUpPage(logger, service))
-	r.Post("/signup", handler.SignUpPage(logger, service))
-
-	r.Group(func(r chi.Router) {
-		r.Use(middlewares.AuthMiddleware)
-
-		r.Get("/skoof", handler.SkoofPage(logger))
-		r.Post("/skoof", handler.SkoofPage(logger))
-	})
-
-	srv := http.Server{
-		Addr:    config.HTTPConfig.Addr,
-		Handler: r,
-	}
+	// initializes middlewares
+	mdwrs(r, middleware.RequestID, middleware.Logger, httprate.Limit(5, time.Second))
+	// initializes server routes and returns a completed http server
+	srv := handler.Routes(r, logger, authService, config)
 	log.Println("server running at", config.HTTPConfig.Addr)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func mdwrs(r *chi.Mux, middlewares ...func(http.Handler) http.Handler) {
+	for _, mdwr := range middlewares {
+		r.Use(mdwr)
+	}
+}
+
+func must[T any](v T, err error) T {
+	if err != nil {
+		panic(err)
+	}
+
+	return v
 }
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -1,1 +1,38 @@
 package handler
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/gogapopp/Skoof/internal/config"
+	"github.com/gogapopp/Skoof/internal/handler/middlewares"
+	"go.uber.org/zap"
+)
+
+const ReadHeaderTimeout = 10 * time.Second
+
+func Routes(r *chi.Mux, logger *zap.SugaredLogger, authService authService, config *config.Config) *http.Server {
+	r.Get("/", HomePage(logger))
+
+	r.Get("/signin", SignInPage(logger, authService))
+	r.Post("/signin", SignInPage(logger, authService))
+
+	r.Get("/signup", SignUpPage(logger, authService))
+	r.Post("/signup", SignUpPage(logger, authService))
+
+	r.Group(func(r chi.Router) {
+		r.Use(middlewares.AuthMiddleware)
+
+		r.Get("/skoof", SkoofPage(logger))
+		r.Post("/skoof", SkoofPage(logger))
+	})
+
+	srv := &http.Server{
+		Addr:              config.HTTPConfig.Addr,
+		Handler:           r,
+		ReadHeaderTimeout: ReadHeaderTimeout,
+	}
+
+	return srv
+}
